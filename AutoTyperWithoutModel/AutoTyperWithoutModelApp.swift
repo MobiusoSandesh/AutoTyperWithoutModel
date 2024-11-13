7e6cd0973cebc33fa49d7115aca0f6f7cea77054
MobiusoSandesh <sandesh@mobiuso.com>
2024-11-13 15:28:42 +0530

    Initial Commit

---
+++ AutoTyperWithoutModel/AutoTyperWithoutModelApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoTyperWithoutModelApp.swift
+//  AutoTyperWithoutModel
+//
+//  Created by Sandesh on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AutoTyperWithoutModelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
